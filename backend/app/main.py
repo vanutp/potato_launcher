c2ab967543754d6593fbba049f13feec49053fc6
Nikita Rakcheev <rakcheev@intervolga.ru>
2025-11-11 15:57:28 +0300

    BC-2: Mock for Getting Modpacks.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes import auth
+from app.routes import modpack
 
 app = FastAPI()
 
-app.include_router(auth.router)
+app.include_router(auth.router, prefix="/api/v1")
+app.include_router(modpack.router, prefix="/api/v1")
